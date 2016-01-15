67206aef8e52e9e3f76c7da6796f78012dd919ec
Chantry <gbdany18@gmail.com>
2016-01-15 14:20:28 -0600

    00-Saludo

---
+++ 00-Saludo/src/Saludo.java
@@ -0,0 +1,20 @@
+/**
+ * Daniela Garcia
+ */
+
+import javax.swing.JOptionPane;
+
+public class Saludo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		JOptionPane.showMessageDialog(null, "Hola Dany"); // ventana en forma grafica
+		
+		System.out.println("Hola desde la consola");
+	
+
+	}
+
+}
